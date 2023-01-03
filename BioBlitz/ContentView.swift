2b7d25135b11d481bb4b2e792422d9a8fb77b458
Kali Sina <ali.sina89@yahoo.com>
2023-01-03 23:31:54 +0100

    Start project

--- BioBlitz/ContentView.swift
+++ BioBlitz/ContentView.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  ContentView.swift
 //  BioBlitz
 //
@@ -10,12 +10,28 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Text("GREEN: 0")
+                Spacer()
+                Text("BIOBLITZ")
+                Spacer()
+                Text("RED: 0")
+            }
+            .font(.system(size: 36).weight(.black))
+            
+            VStack {
+                ForEach(0..<11, id: \.self) { row in
+                    HStack {
+                        ForEach(0..<22, id: \.self) { col in
+                            Text("X")
+                        }
+                    }
+                }
+            }
         }
         .padding()
+        .fixedSize()
+        .preferredColorScheme(.dark)
     }
 }
 
