fe935096ce16b8f0e698e95ca797d93648d60cb9
Kali Sina <ali.sina89@yahoo.com>
2023-01-03 23:05:04 +0100

    Initial Commit

---
+++ BioBlitz/BioBlitzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioBlitzApp.swift
+//  BioBlitz
+//
+//  Created by Triumph on 03/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BioBlitzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
